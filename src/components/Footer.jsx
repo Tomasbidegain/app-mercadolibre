68208220dd9bbdd8023b577940c1a1ae8df0ef35
Tomasbidegain <tomasbidegain8888@gmail.com>
2021-11-02 00:36:10 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <div className="footer">
+            TP Final React App MercadoLibre | Desarrolado por Bidegain Tomás
+        </div>
+     );
+}
+ 
+export default Footer;
